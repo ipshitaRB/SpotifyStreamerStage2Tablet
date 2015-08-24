abab2b5054574400d9afd5d24c07c0e2ee8ae84f
ipshitaRB <iroyburman89@gmail.com>
2015-08-23 18:55:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ipshita/spotifystreamerstage2/adapters/viewholder/TopTracksViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.ipshita.spotifystreamerstage2.adapters.viewholder;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Ipshita on 12-07-2015.
+ */
+public class TopTracksViewHolder {
+    public TextView trackName;
+    public TextView albumName;
+    public ImageView albumThumbnail;
+}
